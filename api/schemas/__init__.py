63cd6a4d4af63e32d4e8ef1db32b3c687872bf65
S3GFelipe <122311328+S3GFelipe@users.noreply.github.com>
2023-12-09 12:35:23 -0300

    Delete api directory

--- api/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from schemas.equipamento_schema import EquipamentoSchema,  EquipamentoBuscaSchema, EquipamentoViewSchema, EquipamentoDelSchema, \
-                                    apresenta_equipamentos, apresenta_equipamento
-                                        
-from schemas.error_schema import ErrorSchema
-                                    
